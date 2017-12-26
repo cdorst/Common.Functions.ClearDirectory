5be67294ca097d0785e515adb8b06adcaa166e97
Christopher Dorst <chris@dorst.email>
2017-12-25 18:55:15 -0600

    Adding DirectoryClearer

---
+++ Common.Functions.ClearDirectory/DirectoryClearer.cs
@@ -0,0 +1,19 @@
+﻿using Common.Functions.ResolveRelativePath;
+using static Common.Functions.CreateDirectory.DirectoryCreator;
+using static System.IO.Directory;
+
+namespace Common.Functions.ClearDirectory
+{
+    public static class DirectoryClearer
+    {
+        public static void ClearDirectory(string path)
+        {
+            if (Exists(path)) Delete(path, recursive: true);
+            Create(path);
+        }
+
+        public static void ClearRelativeDirectory(string pathRelativeToCurrent)
+            => ClearDirectory(
+                RelativePathResolver.GetPath(pathRelativeToCurrent));
+    }
+}
