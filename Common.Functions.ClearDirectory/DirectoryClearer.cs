017bdf9792889ba6564d5def8be3d3608c2a5c26
Christopher Dorst <chris@dorst.email>
2017-12-26 11:16:46 -0600

    Renamed and merged methods

--- Common.Functions.ClearDirectory/DirectoryClearer.cs
+++ Common.Functions.ClearDirectory/DirectoryClearer.cs
@@ -1,19 +1,16 @@
-﻿using Common.Functions.ResolveRelativePath;
-using static Common.Functions.CreateDirectory.DirectoryCreator;
+﻿using static Common.Functions.CreateDirectory.DirectoryCreator;
+using static Common.Functions.ResolveRelativePath.RelativePathResolver;
 using static System.IO.Directory;
 
 namespace Common.Functions.ClearDirectory
 {
     public static class DirectoryClearer
     {
-        public static void ClearDirectory(string path)
+        public static void Clear(string path, bool relativePath = false)
         {
+            if (relativePath) path = GetPath(path);
             if (Exists(path)) Delete(path, recursive: true);
             Create(path);
         }
-
-        public static void ClearRelativeDirectory(string pathRelativeToCurrent)
-            => ClearDirectory(
-                RelativePathResolver.GetPath(pathRelativeToCurrent));
     }
 }
